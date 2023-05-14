dcfd570a6397dd205d938b7fa4b0c3be7d30e33f
Aarun Jury <aarunjury@me.com>
2023-05-14 17:27:45 +1000

    Add Topic 2

---
+++ week3/topic2/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1 style="color:red;"> Hello World!<h1>'
+
+@app.route('/secret')
+def secret():
+    return '<h1 style="color:yellow;">You found the secret page</h1>'
+
+if __name__ == '__main__':
+    app.run(debug = True)
