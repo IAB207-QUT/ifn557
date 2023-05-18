3650c9677472bd9d7ef7637061523291ca192453
Aarun Jury <aarunjury@me.com>
2023-05-18 19:01:33 +1000

    Minor edit

--- week3/topic1/miltontours/order.py
+++ week3/topic1/miltontours/order.py
@@ -1,4 +1,5 @@
 class Order:
+
     def __init__(self, status, firstname, surname, email, phone, date, tours, total_cost):
         self.status = status
         self.firstname = firstname
@@ -13,6 +14,6 @@ class Order:
         return str(self)
 
     def __repr__(self):
-        str = "Status: {}, Firstname: {}, Surname: {}, Email: {}, Phone: {}, Date: {}, Tours: {}, Total Cost: {}\n" 
-        str = str.format( self.status,self.firstname,self.surname, self.email, self.phone, self.date, self.tours, self.total_cost)
+        str = "Status: {}, First Name: {}, Surname: {}, Email: {}, Phone: {}, Date: {}, Tours: {}, Total Cost: {}\n" 
+        str = str.format(self.status, self.firstname, self.surname, self.email, self.phone, self.date, self.tours, self.total_cost)
         return str
