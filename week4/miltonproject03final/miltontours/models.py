cabecfd852893e63523b125f56a8358373b1d71a
Aarun Jury <aarunjury@me.com>
2023-10-20 15:55:50 +1000

    bug fixes

--- week4/miltonproject03final/miltontours/models.py
+++ week4/miltonproject03final/miltontours/models.py
@@ -33,11 +33,11 @@ class Order(db.Model):
     __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.Boolean, default=False)
-    first_name = db.Column(db.String(64))
+    firstname = db.Column(db.String(64))
     surname = db.Column(db.String(64))
     email = db.Column(db.String(128))
     phone = db.Column(db.String(32))
-    total_cost = db.Column(db.Float)
+    totalcost = db.Column(db.Float)
     date = db.Column(db.DateTime)
     tours = db.relationship("Tour", secondary=orderdetails, backref="orders")
     
